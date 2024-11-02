e3952ccd665ac1924cf672c6eb5cbe4970813ba0
Sundeep <sundeep.kaler@gmail.com>
2024-11-02 19:01:52 -0400

    initial commit

---
+++ client/src/components/App.js
@@ -0,0 +1,12 @@
+import React, {useEffect, useState} from "react";
+import {Outlet} from "react-router-dom"
+
+function App() {
+    return <div>
+        <main>
+
+        </main>
+    </div>
+}
+
+export default App;
---
+++ client/src/routes.js
@@ -0,0 +1,13 @@
+import App from './components/App';
+
+const routes = [
+    {
+        path: '/',
+        element: <App />,
+        children: [
+
+        ]
+    }
+]
+
+export default routes;
